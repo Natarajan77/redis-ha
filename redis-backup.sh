69207f69de156a15db3f42f13ca6e99652a7c2d7
Natarajan77 <35440545+Natarajan77@users.noreply.github.com>
2018-05-16 22:42:00 +0000

    Update redis-backup.sh

--- redis-backup.sh
+++ redis-backup.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-export REDIS_MASTER_IP=$(redis-cli -h backend-redis-sentinel -p 26379 --csv SENTINEL get-master-addr-by-name mymaster |awk -F, '{print $1}'|sed -e 's/^"//' -e 's/"$//') 
-echo $REDIS_MASTER_IP
-export REDIS_BACKUP_STATUS=$(redis-cli -h $REDIS_MASTER_IP -p 6379 bgsave) 
-echo $REDIS_BACKUP_STATUS
-date -d @$(redis-cli -h $REDIS_MASTER_IP -p 6379 lastsave | cut -d ' ' -f1) 
-export REDIS_MASTER=$(oc get pods -lapp=backend-redis -o custom-columns=NAME:.metadata.name,IP:.status.podIP --no-headers | grep $REDIS_MASTER_IP| cut -d ' ' -f1)
+#!/bin/bashexport REDIS_MASTER_IP=$(/opt/rh/rh-redis32/root/usr/bin/redis-cli -h backend-redis-sentinel -p 26379 --csv SENTINEL get-master-addr-by-name mymaster |awk -F, '{print $1}'|sed -e 's/^"//' -e 's/"$//') echo $REDIS_MASTER_IPexport REDIS_BACKUP_STATUS=$(/opt/rh/rh-redis32/root/usr/bin/redis-cli -h $REDIS_MASTER_IP -p 6379 bgsave) echo $REDIS_BACKUP_STATUSdate -d @$(/opt/rh/rh-redis32/root/usr/bin/redis-cli -h $REDIS_MASTER_IP -p 6379 lastsave | cut -d ' ' -f1) 
+
